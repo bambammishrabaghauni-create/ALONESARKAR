b9e1b256dffe4d0061a17ebeed68fc29c763d542
ʙᴀᴅ ᴍᴜɴᴅᴀ ❤️ <kiran6jeet8@gmail.com>
2026-06-02 13:36:36 +0530

    Remove source button for private rows
    Removed source button from private rows in _inline.py

--- AloneX/helpers/_inline.py
+++ AloneX/helpers/_inline.py
@@ -192,11 +192,8 @@ class Inline:
             rows += [
                 [
                     self.ikb(text=lang["aloneowner"], user_id=config.OWNER_ID, style=ButtonStyle.DANGER),
-                    self.ikb(
-                        text=lang["source"],
-                        url="\x68\x74\x74\x70\x73\x3A\x2F\x2F\x67\x69\x74\x68\x75\x62\x2E\x63\x6F\x6D\x2F\x54\x65\x61\x6D\x41\x6C\x6F\x6E\x65\x4F\x70\x2F\x41\x6C\x6F\x6E\x65\x58", style=ButtonStyle.DANGER
-                    )
-                ]
+        
+                ],
             ]
         else:
             rows += [[self.ikb(text=lang["language"], callback_data="language")]]
